08ab7d4c98b23ad488dfeca1ae1cf6f539d50a47
goiastrolol <thiagoguedes157@hotmail.com>
2017-06-06 22:31:02 -0300

    bla

--- ProjetoFrotaDeCarros/src/persistencia/clienteDAO.java
+++ ProjetoFrotaDeCarros/src/persistencia/clienteDAO.java
@@ -13,14 +13,15 @@ public class clienteDAO {
         cadastroCliente objCliente = (cadastroCliente) (objeto);
         FileWriter fileCliente = null;
         BufferedWriter bufferCliente = null;
-        
+        String quebraLinha = System.getProperty("line.separator");
         try {
             fileCliente = new FileWriter(arquivoDeCliente,true);
             bufferCliente = new BufferedWriter(fileCliente);
             String aux1 = objCliente.getNomeDoCliente()+","+objCliente.getCPF()+","+ objCliente.getEndereco()+","+
-            objCliente.getCNH()+","+ objCliente.getTelRes()+","+ objCliente.getTelCel()+"\n";
+            objCliente.getCNH()+","+ objCliente.getTelRes()+","+ objCliente.getTelCel()+quebraLinha;
             bufferCliente.write(aux1);
             bufferCliente.close();
+            
         } catch (Exception e) {
         }
 }
